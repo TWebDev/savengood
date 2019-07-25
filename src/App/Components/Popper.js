3152c11d15ec8664ec1b2438f3ca3db01278b01e
TWebDev <52970731+TWebDev@users.noreply.github.com>
2019-07-25 15:55:41 -0500

    Reajuste

--- src/App/Components/Popper.js
+++ src/App/Components/Popper.js
@@ -21,37 +21,37 @@ const Popper = () => {
     Ratio: '1by1',
     Source: couple,
     Alt: 'ByThePool',
-    children: <h1 className='title has-text-centered  has-text-white'><span>R</span> elax by the <span>p</span>ool</h1>
+    children: <h1 className='title has-text-centered  has-text-white'><span>R</span>elax by the <span>p</span>ool</h1>
   },
   {
     Ratio: '9by16',
     Source: jimmy,
     Alt: 'Running',
-    children: <h1 className='title has-text-centered has-text-white'><span>G</span> o for a <span>r</span>un</h1>
+    children: <h1 className='title has-text-centered has-text-white'><span>G</span>o for a <span>r</span>un</h1>
   },
   {
     Ratio: '1by1',
     Source: malecon,
     Alt: 'malecon',
-    children: <h1 className='title has-text-centered has-text-white'><span>C</span> hill at the <span>m</span>alecon</h1>
+    children: <h1 className='title has-text-centered has-text-white'><span>C</span>hill at the <span>m</span>alecon</h1>
   },
   {
     Ratio: '1by2',
     Source: mango,
     Alt: 'mango',
-    children: <h1 className='title has-text-centered has-text-white'><span>T</span> aste a <span>m</span>ango</h1>
+    children: <h1 className='title has-text-centered has-text-white'><span>T</span>aste a <span>m</span>ango</h1>
   },
   {
     Ratio: '1by1',
     Source: massage,
     Alt: 'massage-bed',
-    children: <h1 className='title has-text-centered has-text-white'><span>G</span> et a <span>m</span>assage</h1>
+    children: <h1 className='title has-text-centered has-text-white'><span>G</span>et a <span>m</span>assage</h1>
   },
   { 
     Ratio: '4by3',
     Source: wedding,
     Alt: 'marriage-proposal',
-    children: <h1 className='title has-text-centered has-text-white'><span>B</span> eing proposed <span>m</span>arriage</h1>
+    children: <h1 className='title has-text-centered has-text-white'><span>B</span>eing proposed <span>m</span>arriage</h1>
   }]
   
   return ( 
